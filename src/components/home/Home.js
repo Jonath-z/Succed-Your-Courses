3099a77e312d082b0a62a1c96a593d2f9cdfd299
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-10-28 20:40:20 +0200

    subscription to a course setted

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <div className='absolute top-0 left-0 right-0 bg-pink-900 bottom-0 text-white'>
                 <User />
             </div>
-            <div className='absolute bottom-0  left-0 right-0 bg-white pb-20 rounded-tl-3xl rounded-tr-3xl '>
+            <div className='absolute bottom-0  left-0 right-0 top-60 bg-white pb-20 rounded-tl-3xl rounded-tr-3xl '>
                 <Courses/>
             </div>
         </div>
