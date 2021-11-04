8df3c27ac99244c979d5fe4cfff44fdc0196b228
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-11-04 10:18:42 +0200

    tailwind config file

--- postcss.config.js
+++ postcss.config.js
@@ -18,18 +18,4 @@ module.exports = {
             }
         }
     }
-    // theme: {
-    //     extend: {
-    //       animation: {
-    //           wiggle: 'wiggle 1s ease-in-out infinite',
-    //       }
-    //     }
-    //   }
 };
-
-// module.exports = {
-
-// };
-// module.exports = {
-
-// }
