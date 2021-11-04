8471f9e357e7c138cbb5b2b4240766749c893948
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-11-04 10:19:12 +0200

    dashbord route added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Login from './components/login/Login';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/home/Home';
+import Dashbord from './components/dashbord/Dashbord';
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
             <Login />
           </div>
         </Route>
-        <Route path='/welcome' component={Home}/>
+        <Route path='/welcome' component={Home} />
+        <Route path='/dashbord' component={Dashbord}/>
       </>
     </Router>
   );
