0f710f44dbaef71740f0f50178f2e302170e39ef
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2022-01-11 19:04:03 +0200

    loginFom,loginButton and redux added

--- src/components/Auth-user/login/Login.js
+++ src/components/Auth-user/login/Login.js
@@ -6,66 +6,67 @@ import firebase, { signInwithGoogle,fireStoreDB } from '../../modules/firebase';
 import uuid from 'react-uuid';
 import CryptoJS from 'crypto-js';
 import { useHistory } from 'react-router-dom';
-
+import LoginForm from './tools/_form/LoginForm';
+import LoginButton from './tools/button/LoginButton';
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionsCreators } from '../../../state';
 
 const Login = () => {
     const [isWelcome, setIsWelcome] = useState(true);
-    let history = useHistory();
-    useEffect(() => {
-        const setTime = () => {
-            setIsWelcome(false);
-        }
-        setTimeout(setTime, 3000);
-    }, []);
+    const dispatch = useDispatch();
+    const { authLogin } = bindActionCreators(actionsCreators, dispatch);
+    // console.log(authSignup);
+    // let history = useHistory();
+    // useEffect(() => {
+    //     const setTime = () => {
+    //         setIsWelcome(false);
+    //     }
+    //     setTimeout(setTime, 3000);
+    // }, []);
     
-    useEffect(() => {
-        const userID = uuid();
-        firebase.auth().onAuthStateChanged(user => {
-            fireStoreDB.collection('users').where("email", "==", `${user.email}`)
-                .get()
-                .then((snapshot) => {
-                    if (snapshot.empty) {
-                        console.log(snapshot.empty);
-                        fireStoreDB.collection('users').add({
-                            name: user.displayName,
-                            email: user.email,
-                            profile: user.photoURL,
-                            id: userID,
-                            courses: []
-                        })
-                        history.push(`/welcome/?id=${CryptoJS.AES.encrypt(userID, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`)}`);
-                        localStorage.setItem('userID',CryptoJS.AES.encrypt(userID, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`));
-                    }
-                    if(!snapshot.empty) {
-                        snapshot.forEach(doc => {
-                            console.log(doc.data());
-                            history.push(`/welcome/?id=${CryptoJS.AES.encrypt(doc.data().id, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`)}`);
-                            localStorage.setItem('userID',CryptoJS.AES.encrypt(userID, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`));
-                        })
-                    }
-                }).catch(err => console.log(err))
-            // console.log(user);
-        })
-    }, [history]);
+// ?    useEffect(() => {
+//     const userID = uuid();
+//     firebase.auth().onAuthStateChanged(user => {
+//         fireStoreDB.collection('users').where("email", "==", `${user.email}`)
+//             .get()
+//             .then((snapshot) => {
+//                 if (snapshot.empty) {
+//                     console.log(snapshot.empty);
+//                     fireStoreDB.collection('users').add({
+//                         name: user.displayName,
+//                         email: user.email,
+//                         profile: user.photoURL,
+//                         id: userID,
+//                         courses: []
+//                     })
+//                     history.push(`/welcome/?id=${CryptoJS.AES.encrypt(userID, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`)}`);
+//                     localStorage.setItem('userID',CryptoJS.AES.encrypt(userID, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`));
+//                 }
+//                 if(!snapshot.empty) {
+//                     snapshot.forEach(doc => {
+//                         console.log(doc.data());
+//                         history.push(`/welcome/?id=${CryptoJS.AES.encrypt(doc.data().id, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`)}`);
+//                         localStorage.setItem('userID',CryptoJS.AES.encrypt(userID, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`));
+//                     })
+//                 }
+//             }).catch(err => console.log(err))
+//         // console.log(user);
+//     })
+// }, [history]);
     
     return (
         <div>
-            {isWelcome && <Welcome />}
-            {!isWelcome &&
-                <div style={{
-                    position: 'absolute',
-                    top: '50%',
-                    transform: 'translateY(-50%)',
-                    width: '100%'
-                }}>
-                <button onClick={signInwithGoogle}><FcGoogle
-                 style={{
-                     fontSize: 20,
-                     paddingRight: 10
-                     
-                 }}></FcGoogle>Login with google</button>
+            <div className='flex flex-col justify-center items-center'>
+                <Welcome />
+                <div>
+                    <LoginForm />
                 </div>
-            }
+                <div>
+                    <p className='font-Mulish mt-5'>Don't have an account? <span className='font-Poppins text-2778F0 cursor-pointer' onClick={() => { authLogin(true) }}>Sign up</span></p>
+                </div>
+            </div>
+            <LoginButton/>
         </div>
     );
 }
