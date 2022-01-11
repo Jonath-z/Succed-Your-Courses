49e2035da16b16eb18c7c686ecc3a25458202b33
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2022-01-11 19:05:41 +0200

    redux added

--- src/components/Auth-user/signup/Signup.js
+++ src/components/Auth-user/signup/Signup.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import RegisterButton from './tools/buttons/RegisterButton'
 import SignupForm from './tools/_Forms/SignupForm'
+import { useDispatch } from 'react-redux'
+import { actionsCreators } from '../../../state'
+import { bindActionCreators } from 'redux'
 
 const Signup = () => {
+    const dispatch = useDispatch();
+    const { authSignup } = bindActionCreators(actionsCreators, dispatch);
     return (
         <div>
-            <div>
-                <p>Welcome abroad</p>
+            <div className='flex flex-col justify-center items-center'>
+                <div>
+                    <p className='text-2xl font-extrabold font-Poppins mt-20'>Welcome abroad</p>
+                </div>
+                <div>
+                    <SignupForm />
+                </div>
+                <div>
+                    <p className='font-Mulish mt-5'>Already have an account? <span className='font-Poppins text-2778F0 cursor-pointer' onClick={() => { authSignup(false) }}>Sign in</span></p>
+                </div>
             </div>
-            <div>
-                <SignupForm/>
-            </div>
-            <div>
-                <RegisterButton/>
-            </div>
-            <div>
-                <p>Already have an account? <span>Sign in</span></p>
-            </div>
+            <RegisterButton />
         </div>
-    )
+    );
 }
 
 export default Signup
