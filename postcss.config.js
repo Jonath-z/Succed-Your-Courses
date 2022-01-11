f40eaa2371581b60b30acab6bc13650558562dd0
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2022-01-11 09:02:06 +0200

    custom confiration removed

--- postcss.config.js
+++ postcss.config.js
@@ -5,17 +5,4 @@ module.exports = {
         tailwindcss('./tailwind.js'),
         require('autoprefixer')
     ],
-    theme: {
-        extend: {
-            keyframes: {
-                wiggle: {
-                    '0%': { transform: 'translateY(-100%)' },
-                    '100%': { transform: 'translateY(0)' },
-                }
-            },
-            animation: {
-                'wiggle': 'wiggle 1s ease',
-            }
-        }
-    }
 };
