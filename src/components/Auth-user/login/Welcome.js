5e4d8d0509d28da2297e5d5807b7f8b981ae49f5
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2022-01-11 19:05:14 +0200

    welcome.css deleted

--- src/components/Auth-user/login/Welcome.js
+++ src/components/Auth-user/login/Welcome.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import './Welcome.css';
-import { useEffect } from 'react';
+import React from 'react';
+import { imageObject } from '../../_splash-screen/tools/images';
 
 const Welcome = () => {
-    useEffect(() => {
-        localStorage.clear();
-        },[])
     return (
-        <div className='welcome-container'>
-            <div className='Welcome'>
-                <h1 style={{ fontSize: 30 }} className='welcome-h1'>
-                    Welcome to succed your courses
+        <div className='flex flex-col justify-center items-center'>
+            <div className='text-center'>
+                <h1 className='text-2xl font-extrabold font-Poppins mt-10 pl-2 pr-2'>
+                    Welcome to <span className='text-2778F0'>succed your courses</span>
                 </h1>
-                <p style={{fontSize:20}}>
-                    Let make your student's life easy
+                <p className='font-Mulish'>
+                    Let's make your student's life easy
                 </p>
+            </div>
+            <div>
+                <img src={imageObject.SUCCED_YOUR_COURSES_IMAGE} alt='succed your course'/>
             </div>
         </div>
     );
