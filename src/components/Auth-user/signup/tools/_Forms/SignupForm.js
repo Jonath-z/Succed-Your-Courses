147d94245e09a6fcf64356c384d2e9c74f3708c2
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2022-01-11 19:07:11 +0200

    style added

--- src/components/Auth-user/signup/tools/_Forms/SignupForm.js
+++ src/components/Auth-user/signup/tools/_Forms/SignupForm.js
@@ -3,11 +3,11 @@ import React from 'react'
 const SignupForm = () => {
     return (
         <div>
-            <form>
-                <input type='text' placeholder='Enter your full name' required/>
-                <input type='email' placeholder='Enter your email adress' required />
-                <input type='password' placeholder='Enter password' required/>
-                <input type='password' placeholder='Confirm Pasword'/>
+            <form className='flex flex-col justify-center items-center mt-3'>
+                <input type='text' placeholder='Enter your full name' required className='border border-gray-300 h-10 font-Mulish rounded-3xl pl-7 w-72 mt-5 outlinne-none'/>
+                <input type='email' placeholder='Enter your email adress' className='border border-gray-300 h-10 font-Mulish rounded-3xl pl-7 w-72 mt-5 outlinne-none' required />
+                <input type='password' placeholder='Enter password' className='border border-gray-300 h-10 font-Mulish rounded-3xl pl-7 w-72 mt-5 outlinne-none' required/>
+                <input type='password' placeholder='Confirm Pasword' className='border border-gray-300 h-10 font-Mulish rounded-3xl pl-7 w-72 mt-5 outlinne-none' required/>
             </form>
         </div>
     )
