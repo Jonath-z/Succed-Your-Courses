90931bc7bee5a37c5d0bcf6ee8eb0c920a88d4e3
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2022-01-11 19:08:11 +0200

    react-router-dom history added

--- src/components/_splash-screen/screens/SplashScreen.js
+++ src/components/_splash-screen/screens/SplashScreen.js
@@ -6,22 +6,25 @@ import { lenght } from '../functions/nextHundler';
 import { texts } from '../tools/texts';
 import { nextImageHundler} from '../functions/nextHundler';
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import browserRoutes from '../../../router/_broswerRoute/router';
 // import ProgressPoints from '../tools/progress/ProgressPoints';
 
 const SplashScreen = () => {
     const [imageindex, setimageIndex] = useState(0);
     let image = imagesArray[imageindex];
     let text = texts[imageindex];
+    const history = useHistory();
     return (
         <div>
-            <div className='text-right mr-7 sm:mt-10 xsm:mt-6 font-Mulish'>skip</div>
+            <div className='text-right mr-7 sm:mt-10 xsm:mt-6 font-Mulish cursor-pointer' onClick={()=>browserRoutes.auth(history)}>skip</div>
             <div className='grid grid-rows-2 justify-center items-center'>
                 <div>
                     <img src={image} alt='splash-screen' className='w-90 h-90'/>
                 </div>
                 <div>
-                    <p className='text-2xl font-extrabold font-Poppins'>{text.title}</p><br />
-                    <p className='font-Mulish'>{text.subtitle}</p><br />
+                    <p className='text-2xl font-extrabold font-Poppins pl-1 pr-1'>{text.title}</p><br />
+                    <p className='font-Mulish pl-1 pr-1'>{text.subtitle}</p><br />
                     {/* <ProgressPoints/> */}
                 </div>
             </div>
