7f33a6e307e15ddec65194beb62d68d72542a0ac
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2022-01-14 23:58:00 +0200

    sidebr component added

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiMenu2Line } from 'react-icons/ri';
 import { useSelector } from 'react-redux';
+import SideBar from '../sidBar/SideBar';
+
 
 const User = () => {
+    const [showSidebar, setShowsidebar] = useState(false);
     const user = useSelector((state) => state.fetchUsers);
 
+    const showMenu = () => {
+        setShowsidebar(true);
+    }
+    const hideMenu = () => {
+        setShowsidebar(false);
+    }
+
     return (
-        <div className='flex flex-row justify-between items-center mt-3 mr-3 ml-3'>
-            <RiMenu2Line className='text-3xl cursor-pointer'/>
+        <div className='flex flex-row justify-between items-center mt-3 mr-3 ml-3 pb-3'>
+            <RiMenu2Line className='text-3xl cursor-pointer' onClick={showMenu} />
+            {showSidebar &&
+                <SideBar hideMenu={hideMenu} />
+            }
             <div>
                 <p className='font-Poppins'>Explore Courses</p>
             </div>
