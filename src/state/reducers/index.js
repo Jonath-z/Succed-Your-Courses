3a76f2a2e129e4dc551a758c9f17512902449391
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2022-01-14 23:59:59 +0200

    leave and enroll reducers added

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -3,12 +3,16 @@ import { authReducer } from "./_auth_reducer/authReducer";
 import formReducer from "./form_Reducer/formReducer";
 import fetchUsersReducer from "./users_reducer/usersReducer";
 import { allCourseReducer } from "./course_reducer/allCourseReducer";
+import enroll from "./course_reducer/enrollReducer";
+import leave from "./course_reducer/leaveCourseReducer";
 
 const reducers = combineReducers({
     auth: authReducer,
     form: formReducer,
     fetchUsers: fetchUsersReducer,
-    coursesReducer: allCourseReducer
+    coursesReducer: allCourseReducer,
+    enroll: enroll,
+    leave: leave
 });
 
 export default reducers;
