feaa85fce528f78f8d085beed4cfc02c3a783ee6
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2022-01-14 23:54:46 +0200

    update data in firestore

--- src/components/courses/functions/enrollHandler/enrollTheCourse.js
+++ src/components/courses/functions/enrollHandler/enrollTheCourse.js
@@ -1,12 +1,12 @@
 import { fireStoreDB } from "../../../modules/firebase"; 
 import firebase from "firebase";
 
-const upadate = (snapshot,courseID) => {
+const update = (snapshot,courseID) => {
     snapshot.forEach(async (doc) => {
         console.log(doc.id);
         const ref = fireStoreDB.collection("/users").doc(`${doc.id}`);
         if (doc.data().courses && doc.data().courses.indexOf(`${courseID}`) === -1) {
-            await ref.set({
+            await ref.update({
                 courses: firebase.firestore.FieldValue.arrayUnion(`${courseID}`)
             }, { merge: true });
         }
@@ -17,7 +17,7 @@ const enrollTheCourse = (courseID, userID) => {
     fireStoreDB.collection("/users").where("id", "==", `${userID}`)
         .get()
         .then((snapshot) => {
-            upadate(snapshot, courseID);
+            update(snapshot, courseID);
         });
 }
 
