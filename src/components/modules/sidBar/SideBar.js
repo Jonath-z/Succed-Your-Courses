ca04b2d44277f9551162476943abfcccdf27e881
Jonath-z <jonathanzihindula95@gmail.com>
2022-01-30 15:17:13 +0200

    useUser context setted

--- src/components/modules/sidBar/SideBar.js
+++ src/components/modules/sidBar/SideBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Menu from './menu';
-import {IoCloseOutline} from 'react-icons/io5'
+import { IoCloseOutline } from 'react-icons/io5';
+import { useUser } from '../../context';
 
 const SideBar = ({hideMenu}) => {
-    const user = useSelector((state) => state.fetchUsers);
+    const user = useUser();
     return (
         <div className='absolute top-5 lef-0 bg-white flex flex-col justify-center items-center pl-5 pr-5 pt-5 pb-5 shadow-2xl rounded-r-lg'>
             <div className='w-full'>
