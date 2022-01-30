0f90d354bfcbba99433f5a9e85498a3a72e381bd
Jonath-z <jonathanzihindula95@gmail.com>
2022-01-30 15:13:51 +0200

    user stored in context API

--- src/components/modules/courses/AllCourses.js
+++ src/components/modules/courses/AllCourses.js
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import enrollCourse from './services/enrollHandler';
+import React from 'react';
+import enrollCourse,{updateLocalStorage} from './services/enrollHandler';
+import { deleteInLocalStorage } from './services/leaveTheCourse';
 import leaveCourse from './services/leaveTheCourse';
 import { LocalStorage } from '../../helper/localStorage';
-
+import { useUser,useUpdateUser } from '../../context';
+ 
 const AllCourses = () => {
     
     const allCourses = JSON.parse(LocalStorage.get('courses'));
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        setUser(JSON.parse(LocalStorage.get('userData')));
-    }, []);
-    
+    const user = useUser();
+    const updateUser = useUpdateUser();
 
     return (
         <div>
@@ -32,12 +30,16 @@ const AllCourses = () => {
                                             user !== null && user.courses.indexOf(course.id) === -1 ? <button className='border border-gray-200 mb-2 pl-2 pr-2 font-Mulish text-sm rounded-lg float-right mr-6 bottom-0'
                                                 onClick={() => {
                                                     enrollCourse(course.id, user.id);
+                                                    updateLocalStorage(course.id);
+                                                    updateUser();
                                                 }}>
                                                 Enroll Now
                                             </button> :
                                                 <button className='border border-red-600 mb-2 pl-2 pr-2 font-Mulish text-sm rounded-lg float-right mr-6 bottom-0'
                                                     onClick={() => {
                                                         leaveCourse(course.id, user.id);
+                                                        deleteInLocalStorage(course.id);
+                                                        updateUser();
                                                     }}
                                                 >
                                                     Leave Now
