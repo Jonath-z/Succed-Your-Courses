aeb7422c9ebe96e4456acc6fe741422d13b5d2de
Jonath-z <jonathanzihindula95@gmail.com>
2022-01-31 17:03:20 +0200

    default route added

--- src/router/_broswerRoute/router.js
+++ src/router/_broswerRoute/router.js
@@ -6,9 +6,13 @@ const homeRoute = (history) => {
     history.push('/home');
 }
 
+const defaulRoute = (history) => {
+    history.push('/');
+}
 
 const browserRoutes = {
     auth: authRoute,
-    home: homeRoute
+    home: homeRoute,
+    defaulRoute: defaulRoute
 }
 export default browserRoutes;
