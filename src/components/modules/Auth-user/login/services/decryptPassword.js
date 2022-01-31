588a6d686a95ad94984fa7f5f8f44b0dbdb2dafb
Jonath-z <jonathanzihindula95@gmail.com>
2022-01-31 16:51:22 +0200

    decryptPassword service renamed

--- src/components/modules/Auth-user/login/services/decryptPassword.js
+++ src/components/modules/Auth-user/login/services/decryptPassword.js
@@ -1,9 +1,9 @@
 import CryptoJS from 'crypto-js';
 
 const decryptPassword = (password) => {
-    const cryptedPassword = CryptoJS.AES.decrypt(`${password}`, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`).toString(CryptoJS.enc.Utf8)
-    // console.log(cryptedPassword);
-    return cryptedPassword;
+    const decryptedPassword = CryptoJS.AES.decrypt(`${password}`, `${process.env.REACT_APP_CRYPTOJS_ENCRYPT_KEY}`).toString(CryptoJS.enc.Utf8)
+    // console.log(decryptedPassword);
+    return decryptedPassword;
 }
 
 export default decryptPassword;
