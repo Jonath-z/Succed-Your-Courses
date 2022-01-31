55c46a028bbc93d403beeb1a93721f709046654b
Jonath-z <jonathanzihindula95@gmail.com>
2022-01-31 16:57:06 +0200

    user can now update password

--- src/components/modules/account/setttings/Security.js
+++ src/components/modules/account/setttings/Security.js
@@ -13,9 +13,12 @@ const Security = () => {
 
     const updatePassword = () => {
         updateNewPassword(password, user.id);
-        setPassword('');
         setTimeout(handleUpdateConfirmation, 2000);
+        setPassword('');
+    }
 
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
     }
 
     return (
@@ -25,15 +28,15 @@ const Security = () => {
                     <p className='font-Poppins flex flex-col'>
                         Update Password
                         <span className='font-Mulish text-xs'>
-                            <input type='password' placeholder='' className='bg-white mt-1 border outline-none w-full pt-1 pb-1 pl-2'></input>
+                            <input type='password' placeholder='' className='bg-white mt-1 border outline-none w-full pt-1 pb-1 pl-2' onChange={handlePassword}></input>
                         </span>
                     </p>
                     <p className='font-Poppins float-right text-xs text-2778F0 pt-2 cursor-pointer mr-3' onClick={updatePassword}>Update</p>
                 </div>
             </div>}
-            {isPasswordUpdated && <div className='flex  shadow-xl border pt-3 pb-3 ml-8 mr-8 rounded-2xl mt-3 justify-center items-center bg-green-600 '>
+            {isPasswordUpdated && <div className='flex  shadow-xl border pt-3 pb-3 ml-8 mr-8 rounded-2xl mt-3 justify-center items-center '>
                 <div className='pl-5 pr-5'>
-                    <p className='font-Poppins text-white'>
+                    <p className='font-Poppins  text-green-600'>
                         Password Updated !
                     </p>
                 </div>
