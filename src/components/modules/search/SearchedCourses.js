85ca8631bfe4a3f190a2f37c33fd2bcff7539800
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-08 20:21:24 +0200

    searchCourse responsive

--- src/components/modules/search/SearchedCourses.js
+++ src/components/modules/search/SearchedCourses.js
@@ -29,46 +29,55 @@ const SearchedCourses = ({inputValue}) => {
 
     return (
         <div>
-            {
-                allCourses.map(course => {
-                    return (
-                            <div
-                                className={'course-container mt-4 shadow-lg border-gray-200 border-2 ml-4 mr-4 rounded-xl overflow-y-scroll'}
-                            >
-                                <div className='grid grid-cols-2 pt-2 pb-3 pl-2'>
-                                    <img src={course.cover} alt='cover' className='rounded-xl' />
-                                    <div className='ml-3 flex flex-col justify-between'>
-                                        <div>
-                                            <p className='courseName font-Poppins'>{course.module}</p>
-                                            <p className='font-Mulish text-xs'>{course.class}</p>
-                                        </div>
-                                        <div>
-                                            {
-                                                user !== undefined && user.courses.indexOf(course.id) === -1 ? <button className='border border-gray-200 mb-2 pl-2 pr-2 font-Mulish text-sm rounded-lg float-right mr-6 bottom-0'
-                                                    onClick={() => {
-                                                        enrollCourse(course.id, user.id);
-                                                        updateLocalStorage(course.id);
-                                                        updateUser();
-                                                    }}>
-                                                    Enroll Now
-                                                </button> :
-                                                    <button className='text-red-600  mb-2 pl-2 pr-2 font-Poppins text-sm rounded-lg float-right mr-6 bottom-0'
+            <div className='2xl:flex 2xl:justify-center xl:flex lg:flex xl:justify-center lg:justify-center'>
+                <p className='font-Poppins 2xl:w-2/6 xl:w-2/3 lg:w-2/3 ml-5 mt-0 mb-0'></p>
+            </div>
+            <div className='2xl:justify-center 2xl:flex xl:justify-center xl:flex lg:justify-center lg:flex'>
+                <div className='2xl:justify-center 2xl:flex 2xl:flex-col 2xl:w-2/4 xl:w-2/4 lg:w-2/3
+                            xl:justify-center lg:justify-center
+                            xl:flex xl:flex-cols lg:flex lg:flex-col xl:mr-4 lg:mr-4 '>
+                    {
+                        allCourses.map(course => {
+                            return (
+                                <div
+                                    className={`course-container mt-4 shadow-lg border-gray-200 border-2 ml-4 mr-4 rounded-xl sm:overflow-y-scroll xsm:overflow-y-scroll md:overflow-y-scroll`}>
+                                
+                                    <div className='grid grid-cols-2 pt-2 pb-3 pl-2'>
+                                        <img src={course.cover} alt='cover' className='rounded-xl' />
+                                        <div className='ml-3 flex flex-col justify-between'>
+                                            <div>
+                                                <p className='courseName font-Poppins'>{course.module}</p>
+                                                <p className='font-Mulish text-xs'>{course.class}</p>
+                                            </div>
+                                            <div>
+                                                {
+                                                    user !== undefined && user.courses.indexOf(course.id) === -1 ? <button className='border border-gray-200 mb-2 pl-2 pr-2 font-Mulish text-sm rounded-lg float-right mr-6 bottom-0'
                                                         onClick={() => {
-                                                            leaveCourse(course.id, user.id);
-                                                            deleteInLocalStorage(course.id);
+                                                            enrollCourse(course.id, user.id);
+                                                            updateLocalStorage(course.id);
                                                             updateUser();
-                                                        }}
-                                                    >
-                                                        Leave Now
-                                                    </button>
-                                            }
+                                                        }}>
+                                                        Enroll Now
+                                                    </button> :
+                                                        <button className='text-red-600  mb-2 pl-2 pr-2 font-Poppins text-sm rounded-lg float-right mr-6 bottom-0'
+                                                            onClick={() => {
+                                                                leaveCourse(course.id, user.id);
+                                                                deleteInLocalStorage(course.id);
+                                                                updateUser();
+                                                            }}
+                                                        >
+                                                            Leave Now
+                                                        </button>
+                                                }
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                    )
-                }).reverse()
-            }
+                            )
+                        }).reverse()
+                    }
+                </div>
+            </div>
         </div>
     );
 }
