085be6f214cb011e84606b6595afbeeacb1f994d
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-08 20:08:33 +0200

    check is module exist

--- src/components/modules/courseContent/index.js
+++ src/components/modules/courseContent/index.js
@@ -11,22 +11,27 @@ import MediaQuery from 'react-responsive';
 
 const CourseContent = () => {
     const [modules, setModules] = useState(null);
-    const [moduleID, setModuleID] = useState('');
+    // const [moduleID, setModuleID] = useState('');
+    const [isModuleExist, setIsModuleExist] = useState(null);
     const [expiredToken, setExpiredToken] = useState(null);
     const { id } = useParams();
     let history = useHistory();
     const user = useUser();
 
     useEffect(() => {
-        setModuleID(id);
+        // setModuleID(id);
         fireStoreDB.collection('/module-doc').where('moduleID', '==', `${id}`)
             .get()
             .then(snapshot => {
                 if (!snapshot.empty) {
                     snapshot.forEach(doc => {
                         setModules(doc.data());
+                        setIsModuleExist(true);
                         console.log(doc.data());
                     });
+                }
+                else {
+                    setIsModuleExist(false)
                 }
             });
     }, [id]);
@@ -51,85 +56,83 @@ const CourseContent = () => {
     return (
         <div>
             {
-                user !== undefined && user.courses.includes(moduleID) ?
-                    (!expiredToken ?
-                        <div className='2xl:flex 2xl:justify-center 2xl:w-full'>
-                            <div className='absolute top-0 left-0 right-0 h-20 bg-2778F0 rounded-bl-3xl rounded-br-3xl 2xl:rounded-none'>
-                                {
-                                    modules !== null &&
-                                    <div className='mt-2'>
-                                        <p>
-                                            <AiOutlineArrowLeft
-                                                className='text-2xl ml-2 text-white mt-1 absolute cursor-pointer 2xl:ml-5 2xl:top-5'
-                                                onClick={() => {
-                                                    history.goBack();
-                                                }}
-                                            />
-                                        </p>
-                                        <h1
-                                            className='text-2xl flex justify-center item-center text-white mt-2 font-Poppins'
-                                        >
-                                            {modules.moduleName}
-                                        </h1>
-                                        <p
-                                            className='text-center text-gray-300 text-xs font-Mulish'
-                                        >
-                                            {modules.class}
-                                        </p>
-                                    </div>
-                                }
-                            </div>
-                            <div className='absolute top-20 left-0 right-0 bottom-0 bg-white-900 mt-5'>
-                                {
-                                    modules !== null && modules.option.map(course => {
-                                        return (
-                                            <div className='2xl:justify-center 2xl:items-center 2xl:flex'>
-                                                <div className='mt-5 pt-3 pb-3 pl-3 pr-1 ml-4  mr-4 shadow-lg border rounded-lg 2xl:w-2/6' key={course.id}>
-                                                    <p className='font-Mulish' onClick={toggleIframe}>{course.description}</p>
-                                                    <div className='Iframe'>
-                                                        <MediaQuery minWidth={300} maxWidth={767}>
-                                                            <iframe
-                                                                title={course.description}
-                                                                src={`${course.file}`}
-                                                                width={`100%`}
-                                                                height="650"
-                                                                allow="autoplay"
-                                                            ></iframe>
-                                                        </MediaQuery>
-                                                        <MediaQuery minWidth={768}>
-                                                            <iframe
-                                                                title={course.description}
-                                                                src={`${course.file}`}
-                                                                width={`100%`}
-                                                                height="700"
-                                                                allow="autoplay"
-                                                            ></iframe>
-                                                        </MediaQuery>
-                                                    </div>
-                                                    
-                                                
+                (!expiredToken ?
+                    (<div className='2xl:flex 2xl:justify-center 2xl:w-full'>
+                        <div className='absolute top-0 left-0 right-0 h-20 bg-2778F0 rounded-bl-3xl rounded-br-3xl 2xl:rounded-none'>
+                            {
+                                modules !== null &&
+                                <div className='mt-2'>
+                                    <p>
+                                        <AiOutlineArrowLeft
+                                            className='text-2xl ml-2 text-white mt-1 absolute cursor-pointer 2xl:ml-5 2xl:top-5'
+                                            onClick={() => {
+                                                history.goBack();
+                                            }}
+                                        />
+                                    </p>
+                                    <h1
+                                        className='text-2xl flex justify-center item-center text-white mt-2 font-Poppins'
+                                    >
+                                        {modules.moduleName}
+                                    </h1>
+                                    <p
+                                        className='text-center text-gray-300 text-xs font-Mulish'
+                                    >
+                                        {modules.class}
+                                    </p>
+                                </div>
+                            }
+                        </div>
+                        <div className='absolute top-20 left-0 right-0 bottom-0 bg-white-900 mt-5'>
+                            {
+                                modules !== null && modules.option.map(course => {
+                                    return (
+                                        <div className='2xl:justify-center 2xl:items-center 2xl:flex'>
+                                            <div className='mt-5 pt-3 pb-3 pl-3 pr-1 ml-4  mr-4 shadow-lg border rounded-lg 2xl:w-2/6' key={course.id}>
+                                                <p className='font-Mulish' onClick={toggleIframe}>{course.description}</p>
+                                                <div className='Iframe'>
+                                                    <MediaQuery minWidth={300} maxWidth={767}>
+                                                        <iframe
+                                                            title={course.description}
+                                                            src={`${course.file}`}
+                                                            width={`100%`}
+                                                            height="650"
+                                                            allow="autoplay"
+                                                        ></iframe>
+                                                    </MediaQuery>
+                                                    <MediaQuery minWidth={768}>
+                                                        <iframe
+                                                            title={course.description}
+                                                            src={`${course.file}`}
+                                                            width={`100%`}
+                                                            height="700"
+                                                            allow="autoplay"
+                                                        ></iframe>
+                                                    </MediaQuery>
                                                 </div>
                                             </div>
-                                        );
-                                    })
-                                }
-                            </div>
-                        </div> :
-                        (
-                            <div className='flex flex-col justify-center items-center absolute w-screen h-screen'>
-                                <img src={imageObject.ACCESS_DENIED} alt='page not found' className='w-96' />
-                                <p className='mt-4 font-Poppins text-center text-sm'>
-                                    Please update Your payement to get access to this course
-                                </p>
-                            </div>
-                        )) :
-                    (
-                        <div className='flex justify-center items-center absolute w-screen h-screen'>
-                            <PageNotFound
-                                statusDescription={'No modules found for this course !'}
-                            />
+                                        </div>
+                                    );
+                                })
+                            }
                         </div>
-                    )
+                        {isModuleExist !== null && !isModuleExist &&
+                            <div className='flex justify-center items-center absolute w-screen h-screen'>
+                                <PageNotFound
+                                    statusDescription={'No modules found for this course !'}
+                                />
+                            </div>
+                        }
+                    </div>) :
+                    (
+                        <div className='flex flex-col justify-center items-center absolute w-screen h-screen'>
+                            <img src={imageObject.ACCESS_DENIED} alt='page not found' className='w-96' />
+                            <p className='mt-4 font-Poppins text-center text-sm'>
+                                Please update Your payement to get access to this course
+                            </p>
+                        </div>
+                    ))
+                
             }
         </div>
     );
